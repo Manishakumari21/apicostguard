13593114bbc927e3c42fc5cde624c53595d33dd7
Manishakumari21 <manishasingh6114@gmail.com>
2026-08-10 19:50:17 +0530

    Configure backend for cloud deployment

--- backend/src/config/constants.rs
+++ backend/src/config/constants.rs
@@ -1,6 +1,6 @@
 pub const APP_NAME: &str = "APICostGuard";
 pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const DEFAULT_HOST: &str = "127.0.0.1";
+pub const DEFAULT_HOST: &str = "0.0.0.0";
 pub const DEFAULT_PORT: u16 = 8080;
 pub const DEFAULT_LOG_LEVEL: &str = "info";
 pub const DEFAULT_CHANNEL_SIZE: usize = 1024;
